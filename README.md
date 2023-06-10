12e85595c7f068179d164b396de2eb3509de7633
Afl0wer <1171847423@qq.com>
2023-06-10 17:50:52 +0800

    initialize project

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # sitedirscan
-A website sensitive directory detection tool.
+一款敏感目录探测小工具
+## 👋Feature:
+
+- 可指定本地文件目录生成待扫描的自定义目录列表进行探测
+- 如未指定本地文件目录，就使用默认目录字典进行探测
+- 多线程探测，可指定线程数量
+- 按ctrl+c可提前终止探测，并返回扫描中断位置
+- 因手动终止或网络中断等原因导致程序提前终止时，支持断点续扫
+- 设置网络请求频率随机延时以应对反爬机制
+
+***
+##  ✨Help menu:
+![Help_Menu](https://xx "Help Menu")  
+## 🚀Usage:
+- 指定要进行目录扫描的web地址，加载默认字典并以默认线程数(3)开始扫描：  
+  
+  `directory_dectect.py --url http://www.xxx.com/`
+---
+- 指定要进行目录扫描的web地址,并指定本地文件目录以遍历生成自定义的文件目录列表，并以默认线程数(3)开始探测：  
+  
+  `directory_dectect.py --url http://www.xxx.com/ --localdir D:\\dict`
+---
+- 指定要进行目录扫描的web地址,加载默认字典并指定上次扫描中断的位置(文件目录)继续扫描：  
+  
+  `directory_dectect.py --url http://www.xxx.com/ --restart /manager/html`
+---
+- 指定要进行目录扫描的web地址,加载默认字典并以设置的线程数开始扫描：  
+  
+  `directory_dectect.py --url http://www.xxx.com/ -t 5`
+## ⚡️Example：
+![example_image](https://xx "example image")  
---
+++ directory_detect.py
@@ -0,0 +1,136 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+A website sensitive directory detection tool.
+__author__ = 'Af10wer'
+"""
+import os
+import re
+import sys
+import time
+import queue
+import random
+import signal
+import threading
+import contextlib
+
+import requests,argparse,textwrap
+from urllib.parse import urlparse
+
+FILE_SUFFIX = [".jpg",".gif",".png",".css",".js"]
+web_paths = queue.Queue()
+path = ''
+headers = {
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+}
+
+def collect_paths():
+	for root,dirs,files in os.walk('.'):
+		for f in files:
+			if os.path.splitext(f)[1] in FILE_SUFFIX:
+				continue
+			path = os.path.join(root,f)
+			if path.startswith('.'):
+				path = path[1:]
+			#print(path)
+			web_paths.put(path)
+
+@contextlib.contextmanager
+def chdir(path):
+	origin_dir = os.getcwd()
+	os.chdir(path)
+	try:
+		yield
+	finally:
+		os.chdir(origin_dir)
+
+def load_dirfile(restart=None):
+	with open('dict.txt','r',encoding='utf-8') as f:
+		dirlist = f.read()
+		isrestart = False
+		for item in dirlist.split():
+			if restart is not None:
+				if isrestart:
+					web_paths.put(item)
+				elif item ==restart:
+					isrestart = True
+					print(f'Restart dirlist from:{restart}')
+			else:
+				web_paths.put(item)
+	return web_paths
+
+def site_dir_detect():
+	global path
+	while not web_paths.empty():
+		path = web_paths.get()
+		parseurl = urlparse(base_url)
+		reobj = re.compile(r'^/.*/')
+		base_url_path = reobj.findall(parseurl.path)
+		if path.startswith('/') and base_url.endswith('/'):
+			url = f'{base_url[:-1]}{path}'
+		elif path.startswith('/') and (base_url_path == []) and ('.' not in parseurl.path) and (parseurl.path != '/'):
+			url = f'{base_url}{path}'
+		elif path.startswith('/') and (base_url_path == []) and ('.' in parseurl.path):
+			url = f'{parseurl.scheme}://{parseurl.netloc}{path}'
+		elif path.startswith('/') and (base_url_path != []):
+			url = f'{parseurl.scheme}://{parseurl.netloc}{base_url_path[0][:-1]}{path}'
+		time.sleep(random.randint(0,1))
+		try:
+			res = requests.get(url,headers=headers)
+			if res.status_code == 200:
+				print('[%s] : %s' % (res.status_code,url))
+				with open('result.txt','a') as f:
+						f.write(f'{res.status_code}:{url}\n')
+		except requests.exceptions.ConnectionError as e:
+			print('An error occurred：',e)
+	print('The result has been saved to the local file: result.txt')
+
+def signal_handler(signal,frame):
+    print('You terminated the program prematurely.')
+    print('The currently scanned directory is :',path)
+    sys.exit()
+
+def run(thread):
+	threads = []
+	for i in range(thread):
+		t = threading.Thread(target=site_dir_detect)
+		t.setDaemon(True)
+		threads.append(t)
+		t.start()
+		while True:
+			alive = False
+			for i in range(thread):
+				alive = alive or threads[i].is_alive()
+			if not alive:
+				break
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(
+		description = 'Website Sensitive Directory Detection',
+		formatter_class = argparse.RawDescriptionHelpFormatter,
+		epilog = textwrap.dedent('''Example:
+		directory_dectect.py --url http://xxx.com/ [--localdir D:\\dict]  [--restart /manager/html] [-t 5]
+			''')
+		)
+	parser.add_argument('--url',required=True,help='Target website to scan.')
+	parser.add_argument('--localdir',help='Specify a local directory to generate a custom dictionary.If not specified,use default dictionary.')
+	parser.add_argument('--restart',help='When the network interrupt scan is aborted, specify the last completed directory to continue scanning.')
+	parser.add_argument('-t','--thread',type=int,default=3,help='Specify the number of threads to scan, the default is 3.')
+
+	args = parser.parse_args()
+	base_url = args.url
+	restart = args.restart
+	#当未使用字典文件时，就指定目录生成自定义的字典
+	if args.localdir:
+		with chdir(args.localdir):
+			collect_paths()
+	# 如未指定本地文件目录生成自定义的字典，就使用默认目录字典
+	else:
+		load_dirfile(restart)
+	print('>>> Press Ctrl + C, program can be terminated. <<<')
+	signal.signal(signal.SIGINT,signal_handler)
+	signal.signal(signal.SIGTERM,signal_handler)
+	run(args.thread)
+
+
+
